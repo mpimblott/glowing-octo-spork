e1c30a888c756f01800c9bc7ca5ec16732fd616f
mpimblott <matthew@pimblott.com>
2021-08-30 00:48:12 +0100

    Barebones civ randomiser, page templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, request
 import os
 
 app = Flask(__name__)
@@ -7,6 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('under_construction.html')
+
+
+@app.route('/civ')
+def civ():
+    return render_template('civ.html')
+
+
+@app.route('/civ_result', methods=["POST"])
+def civ_result():
+    no_players = request.form.get("no_players")
+    civ_selection = request.form.get("civ_selection")
+    return render_template('civ_result.html', no_players=no_players, civ_selection=civ_selection)
 
 
 # set secret for session
