fd5753a96fcb3a6fac16c0596d046b465308f755
mpimblott <matthew@pimblott.com>
2021-08-30 08:53:48 +0100

    functioning randomiser without beautified output

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, render_template, request
 import os
+import random
+import csv
+
 
 app = Flask(__name__)
 
@@ -9,16 +12,41 @@ def home():
     return render_template('under_construction.html')
 
 
-@app.route('/civ')
+@app.route('/civ', methods=['GET', 'POST'])
 def civ():
-    return render_template('civ.html')
+    if request.method == 'POST':
+        no_players = int(request.form.get("no_players"))
+        no_choices = int(request.form.get("no_choices"))
+        civ_selection = request.form.get("civ_selection")
+        civ_data = []
+        result = []
+        with open(os.path.join(os.path.dirname(__file__), 'civ_data.txt')) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+                    print(no_players)
+                    print(civ_selection)
+                else:
+                    if civ_selection == "New Only":
+                        print(row[1])
+                        print(type(row[1]))
+                        if row[1] == "1":
+                            civ_data.append(row[0])
+                            line_count += 1
+                    else:
+                        civ_data.append(row[0])
+                        line_count += 1
+            print(f'Processed {line_count - 1} lines.')
+            selection = random.sample(range(0, line_count - 1), no_players*no_choices)
+            for i in selection:
+                result.append(civ_data[i])
 
-
-@app.route('/civ_result', methods=["POST"])
-def civ_result():
-    no_players = request.form.get("no_players")
-    civ_selection = request.form.get("civ_selection")
-    return render_template('civ_result.html', no_players=no_players, civ_selection=civ_selection)
+        return render_template('civ.html', no_players=no_players, civ_selection=civ_selection, civ_result=result)
+    else:
+        return render_template('civ.html')
 
 
 # set secret for session
